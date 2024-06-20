1529afa5401506bcd26a84edf677479968926eb6
Mr. Fluffy <git@sixearsm>
2024-06-20 16:47:21 +0000

    update packages to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "home-made scripts";
 
   inputs = {
-    nixpkgs.url     = github:NixOS/nixpkgs/354184a; # master 2023-12-13
+    nixpkgs.url     = github:NixOS/nixpkgs/938aa157; # nixos-24.05 2024-06-20
     flake-utils.url = github:numtide/flake-utils/c0e246b9;
     hpkgs1.url      = github:sixears/hpkgs1/r0.0.24.0;
 #    hpkgs1.url      = path:/home/martyn/src/hpkgs1/;
